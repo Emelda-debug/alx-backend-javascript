872798f9a7a5f63ec7aee9ba03e6fc2b3a7e1f1f
Emelda-debug <teacayemmie@gmail.com>
2024-06-27 21:54:03 +0200

    corrections

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,27 +1,27 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
-    return this.name;
+    return this._name;
   }
 
   set name(name) {
-    if ((typeof name !== 'string') && !(name instanceof String)) {
+    if ((typeof name !== 'string')) {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
   get length() {
-    return this.length;
+    return this._length;
   }
 
   set length(length) {
-    if ((typeof length !== 'number') && !(length instanceof Number)) {
+    if ((typeof length !== 'number')) {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
@@ -32,9 +32,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
+    if ((students instanceof Array)) {
       throw new TypeError('students must be an array of Strings');
     }
-    this.students = students;
+    this._students = students;
   }
 }
--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,44 +1,44 @@
-import Currency from '3-currency'
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(amount) {
-        if((typeof amount !== 'number') && !(amount instanceof Number)) {
-            throw new TypeError('amount must be a number');
-        }
-        this._amount = amount
+  set amount(amount) {
+    if ((typeof amount !== 'number')) {
+      throw new TypeError('amount must be a number');
     }
-    
-    get currency() {
-        return this._currency;
-    }
+    this._amount = amount;
+  }
 
-    set currency(currency) {
-        if((typeof currency !== 'number') && !(currency instanceof Number)) {
-            throw new TypeError('currency must be a number');
-        }
-        this._currency = currency;
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be instance of Currency');
     }
-    displayFullPrice() {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
     return `${this.amount} ${this.currency} (${this.currency_code})`;
-    }
+  }
 
-    static convertPrice(amount, conversionRate) {
-        if((typeof amount !== 'number') && !(amount instanceof Number)) {
-            throw new TypeError('amount should be a number');
-        }
-        if((typeof conversionRate !== 'number') && !(conversionRate instanceof Number)) {
-            throw new TypeError('conversionRate must be a number');
-        }
-        return amount * conversionRate;
+  static convertPrice(amount, conversionRate) {
+    if ((typeof amount !== 'number')) {
+      throw new TypeError('amount should be a number');
     }
-
+    if ((typeof conversionRate !== 'number')) {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
 }
