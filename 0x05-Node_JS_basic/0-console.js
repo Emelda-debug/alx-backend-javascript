ee850ffe39d787973032b5bfef493b2ae7481745
emelda-debug <teacayemmie@gmail.com>
2024-08-22 19:02:26 +0400

    tasks

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+/**
+ * prints in STDOUT the string argument
+ * @param {string} msg - The message to be displayed.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (msg) => console.log(msg);
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const input_name = process.stdin.read();
+
+  if (input_name) {
+    process.stdout.write(`Your name is: ${input_name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
